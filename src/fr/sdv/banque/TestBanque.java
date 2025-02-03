57ade86266a97ca9e3b4659c84bd242dae0ec6b6
Donovan TOUGERON <schumi.dono@gmail.com>
2025-02-03 11:17:58 +0100

    TP 1 Terminé

--- src/fr/sdv/banque/TestBanque.java
+++ src/fr/sdv/banque/TestBanque.java
@@ -5,5 +5,7 @@ import fr.sdv.banque.entites.Compte;
 public class TestBanque {
     public static void main(String[] args) {
         Compte compte = new Compte("123456789", 1000.01);
+
+        System.out.println(compte);
     }
 }
